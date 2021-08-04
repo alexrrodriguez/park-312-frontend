b35c26e42f5cd4f517ebd56a4feb00fbf96d2c03
alex <arodriguez202020@gmail.com>
2021-08-04 10:04:28 -0500

    add embedded chicago park map to parkIndex page

--- src/views/Park312.vue
+++ src/views/Park312.vue
@@ -1,7 +1,39 @@
 <template>
   <div class="parks-index">
     <h1>Search For Parks In Chicago!</h1>
-    <h5>search by park name, district, facility, or park id:</h5>
+    <div class="info">
+      <!-- <img class="map" src="Chicago_neighborhoods_map.png" alt="" /> -->
+      <div>
+        <p style="margin-bottom: 3px">
+          <a
+            href="https://data.cityofchicago.org/Parks-Recreation/Parks-Chicago-Park-District-Facilities-current-/5yyk-qt9y"
+            target="_blank"
+            style="font-size: 12px; font-weight: bold; text-decoration: none; color: #333333; font-family: arial"
+          >
+            Parks - Chicago Park District Facilities (current)
+          </a>
+        </p>
+        <iframe
+          width="600px"
+          title="Parks - Chicago Park District Facilities (current)"
+          height="400px"
+          src="https://data.cityofchicago.org/w/5yyk-qt9y/3q3f-6823?cur=pBqSF35NdLs&from=root"
+          frameborder="0"
+          scrolling="no"
+        >
+          <a
+            href="https://data.cityofchicago.org/Parks-Recreation/Parks-Chicago-Park-District-Facilities-current-/5yyk-qt9y"
+            title="Parks - Chicago Park District Facilities (current)"
+            target="_blank"
+          >
+            Parks - Chicago Park District Facilities (current)
+          </a>
+        </iframe>
+        <p><a href="http://www.socrata.com/" target="_blank">Powered by Socrata</a></p>
+      </div>
+    </div>
+
+    <h2>Search by park name, district, facility, or park id:</h2>
     <input type="text" v-model="search" placeholder="search..." />
     <div v-for="park in filterBy(parks, search, 'name', 'district', 'facility', 'id')" v-bind:key="park.id">
       <p>
@@ -15,6 +47,13 @@
     </div>
   </div>
 </template>
+
+<style>
+.map {
+  width: 200px;
+  height: 200px;
+}
+</style>
 
 <script>
 import Vue from "vue";
