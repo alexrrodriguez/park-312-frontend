7b739eb7bfa3c863783c2f6e1d92fe43a457444e
alex <arodriguez202020@gmail.com>
2021-08-17 11:33:40 -0500

    update ParkShow.vue page to show other user events underneath park information

--- src/views/ParkShow.vue
+++ src/views/ParkShow.vue
@@ -13,31 +13,11 @@
     <hr />
     <h2>UPCOMING PARK EVENTS:</h2>
     <!-- <p>{{ park.park_events }}</p> -->
-    <div v-if="park.park_events.length > 2">
-      <div>
-        <h3>Event Name: {{ park.park_events[0]["event_name"] }}</h3>
-        <h4>Event Creator: {{ park.users[0]["name"] }}</h4>
-        <p>Email: {{ park.users[0]["email"] }}</p>
-        <p>Date: {{ park.park_events[0]["date"] }}</p>
-        <p>Time: {{ park.park_events[0]["time"] }}</p>
-        <p>Attending: {{ park.park_events[0]["attending"] }}</p>
-      </div>
-      <div>
-        <h3>Event Name: {{ park.park_events[1]["event_name"] }}</h3>
-        <h4>Event Creator: {{ park.users[1]["name"] }}</h4>
-        <p>Email: {{ park.users[1]["email"] }}</p>
-        <p>Date: {{ park.park_events[1]["date"] }}</p>
-        <p>Time: {{ park.park_events[1]["time"] }}</p>
-        <p>Attending: {{ park.park_events[1]["attending"] }}</p>
-      </div>
-      <div>
-        <h3>Event Name: {{ park.park_events[2]["event_name"] }}</h3>
-        <h4>Event Creator: {{ park.users[2]["name"] }}</h4>
-        <p>Email: {{ park.users[2]["email"] }}</p>
-        <p>Date: {{ park.park_events[2]["date"] }}</p>
-        <p>Time: {{ park.park_events[2]["time"] }}</p>
-        <p>Attending: {{ park.park_events[2]["attending"] }}</p>
-      </div>
+    <div v-for="event in events" :key="event.id">
+      <h3>Event Name: {{ event.event_name }}</h3>
+      <p>Date: {{ event.date }}</p>
+      <p>Time: {{ event.time }}</p>
+      <p>Attending: {{ event.attending }}</p>
     </div>
   </div>
 </template>
@@ -48,12 +28,17 @@ export default {
   data: function () {
     return {
       park: {},
+      users: [],
+      events: [],
     };
   },
   created: function () {
     axios.get("/parks/" + this.$route.params.id).then((response) => {
       console.log("park show", response);
       this.park = response.data;
+      this.events = response.data.park_events;
+      this.users = response.data.users;
+      console.log(response.data.park_events);
     });
   },
   methods: {},
