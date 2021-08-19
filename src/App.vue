c520e3918b010e9a446f8cede39c67fee63968a0
alex <arodriguez202020@gmail.com>
2021-08-18 20:33:44 -0500

    adds dark header navbar and update foutes with isLoggedIn method

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,93 @@
 <template>
   <div id="app">
+    <!-- Header
+		============================================= -->
+    <header id="header" class="full-header dark">
+      <div id="header-wrap">
+        <div class="container">
+          <div class="header-row">
+            <!-- Logo
+						============================================= -->
+            <div id="logo">
+              <a href="index.html" class="standard-logo" data-dark-logo="images/logo-dark.png">
+                Park 312
+                <!-- <img src="images/logo.png" alt="Canvas Logo" /> -->
+              </a>
+              <a href="index.html" class="retina-logo" data-dark-logo="images/logo-dark@2x.png">
+                Park 312
+                <!-- <img src="images/logo@2x.png" alt="Canvas Logo" /> -->
+              </a>
+            </div>
+            <!-- #logo end -->
+
+            <div class="header-misc">
+              <!-- Top Search
+							============================================= -->
+              <div id="top-search" class="header-misc-icon">
+                <a href="#" id="top-search-trigger">
+                  <i class="icon-line-search"></i>
+                  <i class="icon-line-cross"></i>
+                </a>
+              </div>
+            </div>
+
+            <div id="primary-menu-trigger">
+              <svg class="svg-trigger" viewBox="0 0 100 100">
+                <path
+                  d="m 30,33 h 40 c 3.722839,0 7.5,3.126468 7.5,8.578427 0,5.451959 -2.727029,8.421573 -7.5,8.421573 h -20"
+                ></path>
+                <path d="m 30,50 h 40"></path>
+                <path
+                  d="m 70,67 h -40 c 0,0 -7.5,-0.802118 -7.5,-8.365747 0,-7.563629 7.5,-8.634253 7.5,-8.634253 h 20"
+                ></path>
+              </svg>
+            </div>
+
+            <!-- Primary Navigation
+						============================================= -->
+            <nav class="primary-menu">
+              <ul class="menu-container">
+                <li class="menu-item">
+                  <a class="menu-link" href="/"><div>Home</div></a>
+                </li>
+                <li class="menu-item">
+                  <a class="menu-link" href="/park_event/new" v-if="isLoggedIn()"><div>Create Park Event</div></a>
+                </li>
+                <li class="menu-item mega-menu">
+                  <a class="menu-link" href="/park_events" v-if="isLoggedIn()"><div>Profile Page</div></a>
+                </li>
+                <li class="menu-item mega-menu">
+                  <a class="menu-link" href="/about"><div>About</div></a>
+                </li>
+                <li class="menu-item mega-menu">
+                  <a class="menu-link" href="/signup" v-if="!isLoggedIn()"><div>Signup</div></a>
+                </li>
+                <li class="menu-item">
+                  <a class="menu-link" href="/login" v-if="!isLoggedIn()"><div>Login</div></a>
+                </li>
+                <li class="menu-item mega-menu">
+                  <a class="menu-link" href="/logout" v-if="isLoggedIn()"><div>Logout</div></a>
+                </li>
+              </ul>
+            </nav>
+            <!-- #primary-menu end -->
+
+            <form class="top-search-form" action="search.html" method="get">
+              <input
+                type="text"
+                name="q"
+                class="form-control"
+                value=""
+                placeholder="Type &amp; Hit Enter.."
+                autocomplete="off"
+              />
+            </form>
+          </div>
+        </div>
+      </div>
+      <div class="header-wrap-clone"></div>
+    </header>
+    <!-- #header end -->
     <div id="nav">
       <router-link to="/">Park312</router-link>
       |
@@ -19,28 +107,7 @@
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
 
 <script>
 export default {
