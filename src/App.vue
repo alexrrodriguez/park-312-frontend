b68562a572b80f6d3771001a2775cfa1dc6bf1f9
alex <arodriguez202020@gmail.com>
2021-08-18 20:54:20 -0500

    add footer and update links and information

--- src/App.vue
+++ src/App.vue
@@ -88,7 +88,7 @@
       <div class="header-wrap-clone"></div>
     </header>
     <!-- #header end -->
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Park312</router-link>
       |
       <router-link to="/park_event/new" v-if="isLoggedIn()">Create Park Event</router-link>
@@ -102,8 +102,265 @@
       <router-link to="/login" v-if="!isLoggedIn()">Login</router-link>
       |
       <router-link to="/logout" v-if="isLoggedIn()">Logout</router-link>
-    </div>
+    </div> -->
     <router-view />
+    <!-- Footer
+		============================================= -->
+    <footer id="footer" class="dark">
+      <div class="container">
+        <!-- Footer Widgets
+				============================================= -->
+        <div class="footer-widgets-wrap">
+          <div class="row col-mb-50">
+            <div class="col-md-6 col-lg-4">
+              <div class="widget clearfix">
+                <h1>Park 312</h1>
+                <!-- <img src="images/footer-widget-logo.png" alt="Image" class="footer-logo" /> -->
+
+                <p>
+                  A Website Built To Help User's
+                  <strong>Search</strong>
+                  For Park Facilities And
+                  <strong>Create</strong>
+                  Park Events In The Wonderful City of Chicago.
+                </p>
+
+                <div style="background: url('images/world-map.png') no-repeat center center; background-size: 100%">
+                  <address>
+                    <strong>Headquarters:</strong>
+                    <br />
+                    312 Address St.
+                    <br />
+                    Chicago, IL 60645
+                    <br />
+                  </address>
+                  <abbr title="Phone Number"><strong>Phone:</strong></abbr>
+                  (1) 8547 632521
+                  <br />
+                  <abbr title="Fax"><strong>Fax:</strong></abbr>
+                  (1) 11 4752 1433
+                  <br />
+                  <abbr title="Email Address"><strong>Email:</strong></abbr>
+                  info@park312.com
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-6 col-lg-4">
+              <div class="widget clearfix">
+                <h4>Client Testimonials</h4>
+
+                <div
+                  class="fslider testimonial no-image bg-transparent border-0 shadow-none p-0"
+                  data-animation="slide"
+                  data-arrows="false"
+                >
+                  <div class="flexslider">
+                    <div class="slider-wrap">
+                      <div class="slide">
+                        <div class="testi-image">
+                          <a href="#"><img src="images/testimonials/3.jpg" alt="Customer Testimonails" /></a>
+                        </div>
+                        <div class="testi-content">
+                          <p>
+                            Similique fugit repellendus expedita excepturi iure perferendis provident quia eaque.
+                            Repellendus, vero numquam?
+                          </p>
+                          <div class="testi-meta">
+                            Steve Jobs
+                            <span>Apple Inc.</span>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="slide">
+                        <div class="testi-image">
+                          <a href="#"><img src="images/testimonials/2.jpg" alt="Customer Testimonails" /></a>
+                        </div>
+                        <div class="testi-content">
+                          <p>
+                            Natus voluptatum enim quod necessitatibus quis expedita harum provident eos obcaecati id
+                            culpa corporis molestias.
+                          </p>
+                          <div class="testi-meta">
+                            Collis Ta'eed
+                            <span>Envato Inc.</span>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="slide">
+                        <div class="testi-image">
+                          <a href="#"><img src="images/testimonials/1.jpg" alt="Customer Testimonails" /></a>
+                        </div>
+                        <div class="testi-content">
+                          <p>
+                            Incidunt deleniti blanditiis quas aperiam recusandae consequatur ullam quibusdam cum libero
+                            illo rerum!
+                          </p>
+                          <div class="testi-meta">
+                            John Doe
+                            <span>XYZ Inc.</span>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="widget clearfix">
+                <a href="#" class="social-icon si-small si-rounded si-facebook">
+                  <i class="icon-facebook"></i>
+                  <i class="icon-facebook"></i>
+                </a>
+
+                <a href="#" class="social-icon si-small si-rounded si-twitter">
+                  <i class="icon-twitter"></i>
+                  <i class="icon-twitter"></i>
+                </a>
+
+                <a href="#" class="social-icon si-small si-rounded si-gplus">
+                  <i class="icon-gplus"></i>
+                  <i class="icon-gplus"></i>
+                </a>
+
+                <a href="#" class="social-icon si-small si-rounded si-pinterest">
+                  <i class="icon-pinterest"></i>
+                  <i class="icon-pinterest"></i>
+                </a>
+
+                <a href="#" class="social-icon si-small si-rounded si-vimeo">
+                  <i class="icon-vimeo"></i>
+                  <i class="icon-vimeo"></i>
+                </a>
+
+                <a href="#" class="social-icon si-small si-rounded si-github">
+                  <i class="icon-github"></i>
+                  <i class="icon-github"></i>
+                </a>
+
+                <a href="#" class="social-icon si-small si-rounded si-yahoo">
+                  <i class="icon-yahoo"></i>
+                  <i class="icon-yahoo"></i>
+                </a>
+
+                <a href="#" class="social-icon si-small si-rounded si-linkedin">
+                  <i class="icon-linkedin"></i>
+                  <i class="icon-linkedin"></i>
+                </a>
+              </div>
+            </div>
+
+            <div class="col-md-6 col-lg-4">
+              <div class="widget quick-contact-widget form-widget clearfix">
+                <h4>Send Message</h4>
+
+                <div class="form-result"></div>
+
+                <form
+                  id="quick-contact-form"
+                  name="quick-contact-form"
+                  action="include/form.php"
+                  method="post"
+                  class="quick-contact-form mb-0"
+                >
+                  <div class="form-process">
+                    <div class="css3-spinner">
+                      <div class="css3-spinner-scaler"></div>
+                    </div>
+                  </div>
+
+                  <div class="input-group mx-auto">
+                    <div class="input-group-text"><i class="icon-user"></i></div>
+                    <input
+                      type="text"
+                      class="required form-control"
+                      id="quick-contact-form-name"
+                      name="quick-contact-form-name"
+                      value=""
+                      placeholder="Full Name"
+                    />
+                  </div>
+                  <div class="input-group mx-auto">
+                    <div class="input-group-text"><i class="icon-email2"></i></div>
+                    <input
+                      type="text"
+                      class="required form-control email"
+                      id="quick-contact-form-email"
+                      name="quick-contact-form-email"
+                      value=""
+                      placeholder="Email Address"
+                    />
+                  </div>
+                  <textarea
+                    class="required form-control input-block-level short-textarea"
+                    id="quick-contact-form-message"
+                    name="quick-contact-form-message"
+                    rows="4"
+                    cols="30"
+                    placeholder="Message"
+                  ></textarea>
+                  <input
+                    type="text"
+                    class="d-none"
+                    id="quick-contact-form-botcheck"
+                    name="quick-contact-form-botcheck"
+                    value=""
+                  />
+                  <input type="hidden" name="prefix" value="quick-contact-form-" />
+                  <button
+                    type="submit"
+                    id="quick-contact-form-submit"
+                    name="quick-contact-form-submit"
+                    class="btn btn-danger m-0"
+                    value="submit"
+                  >
+                    Send Email
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- .footer-widgets-wrap end -->
+      </div>
+
+      <!-- Copyrights
+			============================================= -->
+      <div id="copyrights">
+        <div class="container">
+          <div class="row justify-content-between">
+            <div class="col-12 col-lg-auto text-center text-lg-start">
+              <p class="mb-3">Copyrights &copy; 2021</p>
+              <p class="mb-3">Created by Alexander Rodriguez</p>
+              <a href="https://github.com/alexrrodriguez" class="social-icon si-small si-rounded si-github">
+                <i class="icon-github"></i>
+                <i class="icon-github"></i>
+              </a>
+            </div>
+
+            <div class="col-12 col-lg-auto text-center text-lg-end">
+              <div class="copyrights-menu copyright-links mb-0">
+                <a href="/">Home</a>
+                /
+                <a href="/park_event/new" v-if="isLoggedIn()">Create Park Event</a>
+                /
+                <a href="/park_events" v-if="isLoggedIn()">Profile Page</a>
+                /
+                <a href="/about">About</a>
+                /
+                <a href="/signup" v-if="!isLoggedIn()">Signup</a>
+                /
+                <a href="/login" v-if="!isLoggedIn()">Login</a>
+                /
+                <a href="/logout" v-if="isLoggedIn()">Logout</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- #copyrights end -->
+    </footer>
+    <!-- #footer end -->
   </div>
 </template>
 
